33575e4bebe5a0940146b7e0121b1ada63ebb86e
Michael Stevens <stevensmike36@yahoo.com>
2020-06-08 14:20:23 -0500

    mvp

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,19 @@
 def get_indices_of_item_weights(weights, length, limit):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
-
+    cache = {}
+       
+    for i in range(length):       
+    #loop list - get() key if present         
+        result = cache.get(limit - weights[i])
+      
+        if result != None:
+            # print(i, result)
+            return (i, result)   
+         
+    # add to cache when not in it
+        cache[weights[i]] = i
+        # print(cache)
+            
     return None
+
+if __name__ == "__main__":
+    pass
